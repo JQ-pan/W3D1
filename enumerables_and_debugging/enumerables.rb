9b1ae67733a2ea678818722742ce707dc1dc9688
Jiong Qi <jiongqipan@gmail.com>
2022-11-21 17:14:42 -0500

    my_join started

--- enumerables_and_debugging/enumerables.rb
+++ enumerables_and_debugging/enumerables.rb
@@ -58,6 +58,27 @@ class Array
         end
         arr
     end
+
+    def my_rotate(n = 1)
+        new_arr = self.map {|ele| ele}
+        i = 0
+        while i < n
+            new_arr << new_arr.shift
+            i += 1
+        end
+
+        while i > n
+            new_arr.unshift(new_arr.pop)
+            i -= 1
+        end
+        new_arr
+    end
+
+    def my_join(string)
+        new_str = ""
+        self.my_each {|ele|}
+    end
+
 end
 
 # [1,2,3].my_each{|num| puts num}
@@ -75,12 +96,18 @@ end
 
 #p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
 
-c = [10, 11, 12]
-d = [13, 14, 15]
-p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+# a = [ "a", "b", "c", "d" ]
+# p a.my_rotate         #=> ["b", "c", "d", "a"]
+# p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+# p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+# p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
