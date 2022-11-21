ad7dee7c4d9d44e717bb5489988a973250f7cfad
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-11-21 16:57:20 -0500

    something

--- enumerables_and_debugging/enumerables.rb
+++ enumerables_and_debugging/enumerables.rb
@@ -47,7 +47,17 @@ class Array
         return ret_arr
     end
 
-
+    def my_zip(*arg)
+        arr = self.map{|el| [el]}
+        i = 0
+        while i < self.length
+            arg.each do |smallarr|
+                arr[i] << smallarr[i]
+            end
+            i += 1
+        end
+        arr
+    end
 end
 
 # [1,2,3].my_each{|num| puts num}
@@ -63,4 +73,14 @@ end
 # p a.my_all? { |num| num > 1 } # => false
 # p a.my_all? { |num| num < 4 } # => true
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+#p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
